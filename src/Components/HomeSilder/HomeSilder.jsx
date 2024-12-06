e21456a691632bf57d4c127fb140e42ffdd324f0
Moamen ALaa <89150107+moemen03@users.noreply.github.com>
2024-12-06 20:11:06 +0200

    Update HomeSilder.jsx

--- src/Components/HomeSilder/HomeSilder.jsx
+++ src/Components/HomeSilder/HomeSilder.jsx
@@ -9,7 +9,7 @@ import styles from "./HomeSlider.module.css";
 
 export default function HomeSilder() {
     const silderImages = [
-        { img: image4, id: 1 },
+        { img: "https://images.unsplash.com/photo-1487744480471-9ca1bca6fb7d?q=80&w=2691&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", id: 1 },
         { img: image2, id: 2 },
         { img: image3, id: 3 },
     ];
